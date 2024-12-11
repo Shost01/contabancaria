1644592fb200e67aa901b107f33153a7d0e002ef
Nathan <ribeiro.nathan2002@gmail.com>
2024-12-11 11:56:51 -0300

    Adicionando a classe conta.model ao projeto

--- src/conta/Menu.java
+++ src/conta/Menu.java
@@ -1,13 +1,24 @@
 package conta;
 
 import java.util.Scanner;
-
+import conta.model.Conta;
 import conta.util.Cores;
 
 public class Menu {
 
 	public static void main(String[] args) {
-
+		
+		// Instanciamento | Classe -> Objeto utilizavel
+		Conta c1 = new Conta(1, 123, 1, "Maria das Neves", 10000.0f);
+		c1.visualizar();
+		c1.setSaldo(12000.0f);
+		c1.sacar(12000.0f);
+		c1.visualizar();
+		c1.depositar(5000.0f);
+		c1.visualizar();
+		
+		
+		
 		Scanner leia = new Scanner(System.in);
 
 		int opcao;
